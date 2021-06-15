5ae4badd1a51fbaec7fbf5d105f14418935d173d
Suria R Asai <suria.r.asai@gmail.com>
2021-06-15 14:01:35 +0800

    second commit

--- demo/src/main/java/sg/edu/iss/demo/DemoApplication.java
+++ demo/src/main/java/sg/edu/iss/demo/DemoApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import sg.edu.iss.demo.domain.Student;
 import sg.edu.iss.demo.repo.StudentRepository;
 
 @SpringBootApplication
+
 public class DemoApplication {
 	
 	@Autowired
@@ -29,13 +31,38 @@ public class DemoApplication {
 			Student s1 = new Student("Hou Lu", "Sweet", null, 5000.00, 4.50);
 			Student s2 = new Student("Zerita", "Nice", null, 5000.00, 4.50);
 			Student s3 = new Student("Austin", "Rationlist", null, 5000.00, 4.50);
-			Student s4 = new Student("Benjo", "ExamSmart", null, 5000.00, 5.00);
+			Student s4 = new Student("Benjo", "Smart", null, 5000.00, 5.00);
 			srepo.save(s1);srepo.save(s2);srepo.save(s3);srepo.save(s4);
+			//List All
 			List<Student> list = srepo.findAll();
+			System.out.println("All Records");
 			for (Student student : list) {
 				System.out.println("Record from DB"+student.toString());
 			}
-			
+			// CGPA and fee Finder
+			List<Student> list1 = srepo.findStudentsByCgpaAndFee(5.0, 5000.00);
+			System.out.println("Finder 1");
+			for (Student student : list1) {
+				System.out.println("Record from DB"+student.toString());
+			}
+			// Nick Name finder
+			List<Student> list2 = srepo.queryStudentsByNickName("S%");
+			System.out.println("Finder 2");
+			for (Student student : list2) {
+				System.out.println("Record from DB"+student.toString());
+			}
+			//Name finder
+			List<Student> list3 = srepo.queryStudentsByName("Z%");
+			System.out.println("Finder 3");
+			for (Student student : list3) {
+				System.out.println("Record from DB"+student.toString());
+			}
+			//Name finder
+			List<Student> list4 = srepo.findStudentsByNickNameStartingWith("R");
+			System.out.println("Finder 4");
+			for (Student student : list4) {
+				System.out.println("Record from DB"+student.toString());
+			}
 			     
 		};
 	}
--- demo/src/main/java/sg/edu/iss/demo/repo/StudentRepository.java
+++ demo/src/main/java/sg/edu/iss/demo/repo/StudentRepository.java
@@ -1,9 +1,27 @@
 package sg.edu.iss.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.edu.iss.demo.domain.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+	// CRUD - FindByID FindByOne FindAll save() delete()
+	// Standard Queries
+	// findStudentsByCGPAAndFee
+	List<Student> findStudentsByCgpaAndFee(double cgpa, double fee);
+	// findStudentsByNickName
+	List<Student> findStudentsByNickNameStartingWith(String phrase);
+	
+	//Customized Query using Annotations
+	@Query("Select s from Student s where s.nickName like %?1")
+	List<Student> queryStudentsByNickName(String nn);
+	//Customized Query using Annotations
+	@Query("Select s from Student s where s.name like :name")
+	List<Student> queryStudentsByName(@Param("name") String name);
+	
 }
